f1f4aec70beb8cfda205668cb6397281f6486c82
Joshua Evans <Joshe4512@gmail.com>
2025-10-10 12:54:02 -0400

    "Implement phone number standardization"

--- JoshuaEvans_Assignment_6.py
+++ JoshuaEvans_Assignment_6.py
@@ -19,3 +19,49 @@ while True:
     name_field = fields[0].strip() #Removes spaces before or after the name
     name_parts = name_field.split() # Breaks name into individual words
     name = ' '.join(name_parts).title() # Puts them back together and then capatalizes the name properly
+
+    # --- Phone cleaning ---
+    phone_field = fields[1].strip()
+    digits_only = ""
+    for char in phone_field:
+        if char.isdigit():
+            digits_only += char
+# Loop goes through the number and only keeps digits and only recognizes numbers
+    if len(digits_only) == 11 and digits_only[0] == '1':
+        digits_only = digits_only[1:] # if there is an area code in the number this loop removes it
+
+    if len(digits_only) == 10:
+        area = digits_only[:3]
+        prefix = digits_only[3:6]
+        line_num = digits_only[6:]
+        phone = f"({area}) {prefix}-{line_num}"
+    else:
+        phone = "INVALID"
+        #Loop ensures that the number has ten digits and formats it properly and if not its marked as invalid
+
+    # --- Email cleaning ---
+    email_field = fields[2].strip() #Removes outer spaces
+    email = email_field.lower().replace(' ', '').replace(',', '') #removes spaces and commas
+    if '@' not in email or '.' not in email.split('@')[-1]:
+        email = "INVALID"
+
+    # --- Address cleaning ---
+    address_field = fields[3].strip()
+    parts = [p.strip() for p in address_field.split(',')]
+    cleaned_parts = []
+
+    for part in parts:
+        words = part.split()
+        clean_words = []
+        for w in words:
+            if w.isalpha() and len(w) == 2:
+                clean_words.append(w.upper())  # state abbreviation
+            else:
+                clean_words.append(w.title())  # normal word
+        cleaned_parts.append(' '.join(clean_words))
+
+    address = ', '.join(cleaned_parts)
+
+    contacts.append((name, phone, email, address))
+    count += 1
+
