8d69637e30b57bcfdc08df055098f4acf79d1759
Joshua Evans <Joshe4512@gmail.com>
2025-10-10 12:52:52 -0400

    "Initial setup: Contact data input and parsing"

---
+++ JoshuaEvans_Assignment_6.py
@@ -0,0 +1,5 @@
+print("Enter contact information (format: name|phone|email|address):\n")#AI explained the useage of :\n
+
+contacts = [] #list that stores contacts
+count = 0 #Keeps track of how many contatcts are stored
+
